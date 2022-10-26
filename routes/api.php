c1f4ff84448898a0d7af92e1fed7d3edeb0b0e40
AryaZafri <aryaxszafrie@gmail.com>
2022-10-26 11:36:12 +0700

    second commit

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HeloController;
 use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -30,3 +31,4 @@ Route::get('halo', function(){
 //Route::delete('halocontroller',[HeloController::class, 'destroy]);
 Route::resource('halocontroller',HeloController::class);
 Route::resource('siswa',SiswaController::class);
+Route::resource('books',BookController::class);
